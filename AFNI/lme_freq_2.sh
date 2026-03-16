b6f74e81a673791c77a8d45eb1b80c631ab0c57f
Harryyonss <80230674+Harryyonss@users.noreply.github.com>
2026-03-16 14:38:26 +0900

    Update lme_freq_2.sh

--- AFNI/lme_freq_2.sh
+++ AFNI/lme_freq_2.sh
@@ -1,6 +1,8 @@
-3dLMEr -prefix output_LME_2_fin.nii -jobs 16 \
+3dLMEr -prefix output_LME_2.nii -jobs 16 \
        -model "Freq+(1|Subj)+(1|Subj:Run)" \
        -qVars 'Freq' \
        -gltCode 'Freq_effect' 'Freq :' \
-       -mask /mnt/ext4/TMN/fmri_data/preproc_data/M/TMN01/full_mask.group.M.nii \
+       -mask /mnt/ext4/TMN/fmri_data/preproc_data/M/TMN/full_mask.group.M.nii \
        -dataTable @newDataTable_fin_SK_2.txt
+
+
